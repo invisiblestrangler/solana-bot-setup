6ba30c457e1cdf25b34f3e34ff71e1f1428187b4
invisiblestrangler <104713936+invisiblestrangler@users.noreply.github.com>
2024-04-26 06:07:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script performs system updates, clones a Git repository,
-# and installs Node.js along with npm, then sets up the trading bot.
+# installs Node.js, nvm, and npm, then sets up the trading bot.
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root"
@@ -32,6 +32,17 @@ if ! command -v npm > /dev/null 2>&1; then
     echo "The installed version of npm is:"
     npm -v
 fi
+
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Set up environment variables for nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+echo "Installing and using the latest LTS version of Node.js..."
+nvm install --lts
+nvm use --lts
 
 echo "Installing npm packages required by the trading bot..."
 npm install
